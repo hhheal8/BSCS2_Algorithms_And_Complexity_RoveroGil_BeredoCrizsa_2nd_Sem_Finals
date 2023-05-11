7ce68ebcfa8f1fa1a11df744909750b62969662f
hhheal8 <heeeal01000111@hotmail.com>
2023-05-12 07:02:16 +0800

    Some Changes

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class DatabaseManager:
       password=password,
       database=database
     )
+    
     self.cursor = self.connection.cursor()
     
   def is_database_exists(self, database_name: str) -> bool:
@@ -22,6 +23,7 @@ class DatabaseManager:
       self.cursor.execute(f"SHOW DATABASES LIKE '{database_name}'")
       result = self.cursor.fetchone()
       return result is not None
+    
     except mysql.connector.Error as error:
       print(f"\nERROR CHECKING DATABASE EXISTENCE: {error}\n")
       return False
@@ -30,7 +32,9 @@ class DatabaseManager:
     try:
       self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {FOODCOURT_SEAT_RESERVATION_DATABASE}")
       self.connection.commit()
+      
       print("DATABASE CREATED SUCCESSFULLY.\n")
+      
     except mysql.connector.Error as error:
       print(f"\nERROR CREATING DATABASE: {error}\n")
           
@@ -59,10 +63,12 @@ class DatabaseManager:
       )
       """
     ]
+    
     for query in CREATE_TABLE_QUERIES:
       self.cursor.execute(query)
-    
+      
     self.connection.commit()
+    
     print("TABLES CREATED SUCCESSFULLY.\n")
 
   def insert_data(self, table_name: str, data: List[dict]) -> None:
@@ -77,6 +83,7 @@ class DatabaseManager:
         self.cursor.execute(query)
         self.connection.commit()
       print("Data inserted successfully.")
+      
     except mysql.connector.Error as error:
       print(f"\nERROR INSERTING DATA: {error}\n")
 
@@ -86,6 +93,7 @@ class DatabaseManager:
       rows = self.cursor.fetchall()  
       for row in rows:
         print(row)
+        
     except mysql.connector.Error as error:
       print(f"\nERROR READING DATA FROM: {error}\n")
 
@@ -101,6 +109,7 @@ class DatabaseManager:
       self.cursor.execute(query)
       self.connection.commit()
       print("Data updated successfully.")
+      
     except mysql.connector.Error as error:
       print(f"\nERROR UPDATING DATA: {error}\n")
 
@@ -115,6 +124,7 @@ class DatabaseManager:
       self.cursor.execute(query)
       self.connection.commit()
       print("Data deleted successfully.")
+      
     except mysql.connector.Error as error:
       print(f"\nERROR DELETING DATA: {error}\n")
       
@@ -137,6 +147,7 @@ class DatabaseManager:
       rows = self.cursor.fetchall()
       for row in rows:
         print(row)
+
     except mysql.connector.Error as error:
       print(f"\nERROR EXECUTING ADVANCED QUERY: {error}\n")
           
@@ -150,15 +161,15 @@ def main() -> None:
 
   storename_data = [
     {
-      "store_name_id": "A1001_P_C",
+      "store_name_id": "S01_P_C",
       "store_name": "Potato Corner"
     },
     {
-      "store_name_id": "A1005_K_B_H",
+      "store_name_id": "S05_K_B_H",
       "store_name": "Kirmz B Hub"
     },
     {
-      "store_name_id": "A1006_S_O",
+      "store_name_id": "S06_S_O",
       "store_name": "Sisig-OK"
     }
   ]
@@ -167,15 +178,15 @@ def main() -> None:
 
   data = [
     {
-      "store_name_id": "A1001_P_C",
+      "store_name_id": "S01_P_C",
       "seat_fee": 10.00
     },
     {
-      "store_name_id": "A1005_K_B_H",
+      "store_name_id": "S05_K_B_H",
       "seat_fee": 15.00
     },
     {
-      "store_name_id": "A1006_S_O",
+      "store_name_id": "S06_S_O",
       "seat_fee": 25.00
     }
   ]
@@ -184,22 +195,22 @@ def main() -> None:
 
   reservation_data = [
     {
-      "store_name_id": "A1001_P_C",
+      "store_name_id": "S01_P_C",
       "time_reservation": "2023-05-12 10:00:00",
       "number_customer": 4
     },
     {
-      "store_name_id": "A1001_P_C",
+      "store_name_id": "S01_P_C",
       "time_reservation": "2023-05-12 11:00:00",
       "number_customer": 2
     },
     {
-      "store_name_id": "A1005_K_B_H",
+      "store_name_id": "S05_K_B_H",
       "time_reservation": "2023-05-12 12:00:00",
       "number_customer": 3
     },
     {
-      "store_name_id": "A1006_S_O",
+      "store_name_id": "S06_S_O",
       "time_reservation": "2023-05-12 13:00:00",
       "number_customer": 1
     }
@@ -213,11 +224,11 @@ def main() -> None:
   sql.read_data(FOODCOURT_SEAT_RESERVATION_TABLE3)
 
   print("\n--- Updating Data ---")
-  sql.update_data(FOODCOURT_SEAT_RESERVATION_TABLE1, {"seat_fee": 12.50}, "store_name_id = 'A1001_P_C'")
+  sql.update_data(FOODCOURT_SEAT_RESERVATION_TABLE1, {"seat_fee": 12.50}, "store_name_id = 'S01_P_C'")
   sql.read_data(FOODCOURT_SEAT_RESERVATION_TABLE1)
 
   print("\n--- Deleting Data ---")
-  sql.delete_data(FOODCOURT_SEAT_RESERVATION_TABLE2, "store_name_id = 'A1005_K_B_H'")
+  sql.delete_data(FOODCOURT_SEAT_RESERVATION_TABLE2, "store_name_id = 'S05_K_B_H'")
   sql.read_data(FOODCOURT_SEAT_RESERVATION_TABLE2)
 
   print("\n--- Advanced Query ---")
